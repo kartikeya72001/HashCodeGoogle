bfbcf25f8e670db604b7895f8c515b0e7851ab4f
deathcoder <kartikeya72001@gmail.com>
2020-02-19 13:17:24 +0530

    Arrays

--- HashCodePP.cpp
+++ HashCodePP.cpp
@@ -1,12 +1,33 @@
 #include<iostream>
 using namespace std;
-int ans=0;
+int ans=0;//Answer Variable
+int arr1[10000];
+int arr2[10000];
+int arr3[10000];
+int arr4[10000];
+int PointStartEnd(int *arr, int n, long long int sum)//Array, Types People respectively
+{
+    int Sptr=0;//Start Pointer
+    int Eptr=n;//End Pointer
+    int sumnew=0;//New Sum to be checked
+    int counter=0
+    int bestCase[100000]
+    while(sumnew<=sum)
+    {
+        sumnew=arr[sptr]+arr[Eptr];
+        counter+=2;
+        if(sumnew==sum)
+        {
+
+        }
+    }
+}
 int main()
 {
     //Declaration of Variables
-    int ppl=0;
-    int types=0;
-    int arr[100000];
+    long long int ppl=0;//Number of People
+    int types=0;//Types of People
+    int arr[100000];//Array Of Pizzas
     //Input data
     cin>>ppl;
     cin>>types;
