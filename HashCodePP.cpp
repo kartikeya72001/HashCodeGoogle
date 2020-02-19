93c2b48cf46e14e332b12ac157ce9f62ac9770c5
deathcoder <kartikeya72001@gmail.com>
2020-02-19 14:08:48 +0530

    FirstFunc

--- HashCodePP.cpp
+++ HashCodePP.cpp
@@ -1,26 +1,44 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 int ans=0;//Answer Variable
 int arr1[10000];
+int iter=0;//Iterable for arr1
 int arr2[10000];
 int arr3[10000];
 int arr4[10000];
-int PointStartEnd(int *arr, int n, long long int sum)//Array, Types People respectively
+int PointStartEnd(int *arr, int types, long long int ppl)//Array, Types People respectively
 {
     int Sptr=0;//Start Pointer
-    int Eptr=n;//End Pointer
+    int Eptr=types-1;//End Pointer
     int sumnew=0;//New Sum to be checked
-    int counter=0
-    int bestCase[100000]
-    while(sumnew<=sum)
+    while(Sptr<=Eptr)
     {
-        sumnew=arr[sptr]+arr[Eptr];
-        counter+=2;
-        if(sumnew==sum)
+        sumnew+=arr[Sptr]+arr[Eptr];
+        if(sumnew==ppl)//Checking for Equality Condition
         {
-
+            arr1[iter++]=Eptr;
+            arr1[iter++]=Sptr;
+            sort(arr1,arr1+iter);//Sorting the Array
+            return sumnew;
         }
+        if(sumnew>ppl)
+        {
+            sumnew=sumnew-arr[Sptr];
+            arr[iter-1]=arr[iter];
+            iter--;
+        }
+        if(sumnew<ppl)
+        {
+            arr1[iter]=Sptr;
+            arr1[++iter]=Eptr;
+            iter++;
+        }
+        Sptr++;
+        Eptr--;
     }
+    sort(arr1,arr1+iter);//Final sort Out of the Loop
+    return sumnew;
 }
 int main()
 {
@@ -28,13 +46,20 @@ int main()
     long long int ppl=0;//Number of People
     int types=0;//Types of People
     int arr[100000];//Array Of Pizzas
+    int answer=0;
     //Input data
     cin>>ppl;
     cin>>types;
-    for(int i=0; i<types;i++)
+    for(int i=0;i<types;i++)
     {
         cin>>arr[i];
     }
     //Functions Begin here
+    answer=PointStartEnd(arr, types, ppl);
+    cout<<answer<<endl;
+    for(int i=0;i<iter;i++)
+    {
+        cout<<arr1[i]<<" ";
+    }
     return 0;
 }
