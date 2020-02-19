64e7a6cae612e523753c5f5cb7b434ecfc268b84
deathcoder <kartikeya72001@gmail.com>
2020-02-19 15:32:40 +0530

    iterupdate

--- HashCodePP.cpp
+++ HashCodePP.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int ans=0;//Answer Variable
 int arr1[10000];
-int iter=0;//Iterable for arr1
+int iter1=0;//iter1able for arr1
 int arr2[10000];
 int arr3[10000];
 int arr4[10000];
@@ -17,27 +17,27 @@ int PointStartEnd(int *arr, int types, long long int ppl)//Array, Types People r
         sumnew+=arr[Sptr]+arr[Eptr];
         if(sumnew==ppl)//Checking for Equality Condition
         {
-            arr1[iter++]=Eptr;
-            arr1[iter++]=Sptr;
-            sort(arr1,arr1+iter);//Sorting the Array
+            arr1[iter1++]=Eptr;
+            arr1[iter1++]=Sptr;
+            sort(arr1,arr1+iter1);//Sorting the Array
             return sumnew;
         }
         if(sumnew>ppl)
         {
             sumnew=sumnew-arr[Sptr];
-            arr[iter-1]=arr[iter];
-            iter--;
+            arr[iter1-1]=arr[iter1];
+            iter1--;
         }
         if(sumnew<ppl)
         {
-            arr1[iter]=Sptr;
-            arr1[++iter]=Eptr;
-            iter++;
+            arr1[iter1]=Sptr;
+            arr1[++iter1]=Eptr;
+            iter1++;
         }
         Sptr++;
         Eptr--;
     }
-    sort(arr1,arr1+iter);//Final sort Out of the Loop
+    sort(arr1,arr1+iter1);//Final sort Out of the Loop
     return sumnew;
 }
 int main()
@@ -57,7 +57,7 @@ int main()
     //Functions Begin here
     answer=PointStartEnd(arr, types, ppl);
     cout<<answer<<endl;
-    for(int i=0;i<iter;i++)
+    for(int i=0;i<iter1;i++)
     {
         cout<<arr1[i]<<" ";
     }
