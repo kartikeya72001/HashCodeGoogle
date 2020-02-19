c32fd96a4eb2a4298c60ba2d810e73d812a3af63
deathcoder <kartikeya72001@gmail.com>
2020-02-19 12:52:57 +0530

    Main

--- HashCodePP.cpp
+++ HashCodePP.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 using namespace std;
+int ans=0;
 int main()
 {
+    //Declaration of Variables
+    int ppl=0;
+    int types=0;
+    int arr[100000];
+    //Input data
+    cin>>ppl;
+    cin>>types;
+    for(int i=0; i<types;i++)
+    {
+        cin>>arr[i];
+    }
+    //Functions Begin here
     return 0;
 }
