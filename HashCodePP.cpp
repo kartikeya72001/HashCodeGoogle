cf8ede58a82923ae9e234e99fa5743a43574ce21
deathcoder <kartikeya72001@gmail.com>
2020-02-19 15:54:52 +0530

    ArrayError

--- HashCodePP.cpp
+++ HashCodePP.cpp
@@ -3,10 +3,21 @@
 using namespace std;
 int ans=0;//Answer Variable
 int arr1[10000];
-int iter1=0;//iter1able for arr1
+int iter1=0;//iterate1able for arr1
 int arr2[10000];
+int iter2=0;
 int arr3[10000];
+int iter3=0;
 int arr4[10000];
+int iter4=0;
+int arr5[10000];
+int iter5=0;
+int arr6[10000];
+int iter6=0;
+int arr7[10000];
+int iter7=0;
+int arr8[10000];
+int iter8=0;
 int PointStartEnd(int *arr, int types, long long int ppl)//Array, Types People respectively
 {
     int Sptr=0;//Start Pointer
@@ -33,10 +44,49 @@ int PointStartEnd(int *arr, int types, long long int ppl)//Array, Types People r
             arr1[iter1++]=Eptr;
             arr1[iter1++]=Sptr;
         }
-        Sptr++;
-        Eptr--;
+        if(sumnew<ppl)
+        {
+            Sptr++;
+            Eptr--;
+        }
     }
     sort(arr1,arr1+iter1);//Final sort Out of the Loop
+    return sumnew;
+}
+int PointStartEnd2(int *arr, int types, long long int ppl)//Array, Types People respectively
+{
+    int Sptr=0;//Start Pointer
+    int Eptr=types-1;//End Pointer
+    int sumnew=0;//New Sum to be checked
+    while(Sptr<=Eptr)
+    {
+        sumnew+=arr[Sptr]+arr[Eptr];
+        if(sumnew==ppl)//Checking for Equality Condition
+        {
+            arr2[iter2++]=Sptr;
+            arr2[iter2++]=Eptr;
+
+            sort(arr2,arr2+iter2);//Sorting the Array
+            return sumnew;
+        }
+        if(sumnew>ppl)
+        {
+            sumnew=sumnew-arr[Eptr];
+            iter2--;
+        }
+        if(sumnew<ppl)
+        {
+            arr2[iter2++]=Sptr;
+            arr2[iter2++]=Eptr;
+
+        }
+        if(sumnew<ppl)
+        {
+            Sptr++;
+            Eptr--;
+        }
+    }
+    sort(arr2,arr2+iter2);//Final sort Out of the Loop
     return sumnew;
 }
 int main()
@@ -54,11 +104,11 @@ int main()
         cin>>arr[i];
     }
     //Functions Begin here
-    answer=PointStartEnd(arr, types, ppl);
+    answer=PointStartEnd2(arr, types, ppl);
     cout<<answer<<endl;
-    for(int i=0;i<iter1;i++)
+    for(int i=0;i<iter2;i++)
     {
-        cout<<arr1[i]<<" ";
+        cout<<arr2[i]<<" ";
     }
     return 0;
 }
